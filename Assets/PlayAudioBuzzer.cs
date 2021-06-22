f7d7c8d4caf30492940aa62c4be2d7eb9773c22c
PascalLiPooKim <32938312+PascalLiPooKim@users.noreply.github.com>
2021-06-22 13:43:05 +0100

    Implement code for the first audio source.

--- Assets/PlayAudioBuzzer.cs
+++ Assets/PlayAudioBuzzer.cs
@@ -14,6 +14,7 @@ namespace RosSharp.RosBridgeClient
         private MessageTypes.Std.Float32 closestDistance;
         private bool isMessageReceived;
         AudioSource audioSource;
+        public AudioClip impact;
 
 
         // Start is called before the first frame update
@@ -37,21 +38,39 @@ namespace RosSharp.RosBridgeClient
         protected override void ReceiveMessage(MessageTypes.Std.Float32 closestPointDist)
         {
             closestDistance = closestPointDist;
+            print(closestDistance.data);
             isMessageReceived = true;
         }
 
         private void PlayBuzzer()
         {
-            if (closestDistance.data > 0.0f)
-            {
-                audioSource.pitch = Mathf.Min(1.0f/closestDistance.data, 3);
-                audioSource.Play();
-            }
-            else
-            {
-                audioSource.Stop();
-            }
 
+            
+                if (Mathf.Pow(closestDistance.data, 1f / 2f) > 0.0f && Mathf.Pow(closestDistance.data, 1f / 2f) < 1.3f)
+                {
+                    //audioSource.pitch = Mathf.Min(1.0f/closestDistance.data, 3);
+
+                    audioSource.pitch = Mathf.Clamp(1.0f / Mathf.Pow(closestDistance.data, 1f / 9f) - 0.5f, 0.1f, 1.5f);
+                    // audioSource.Play();
+                    audioSource.PlayOneShot(impact, 1.0F);
+
+
+                }
+
+                else
+                {
+                    //audioSource.Stop();
+                    // audioSource.pitch = 1.0f;
+                    audioSource.pitch = 0.0f;
+                }
+
+                
+            
+
+            
+
+
+            // audioSource.Play();
             isMessageReceived = false;
 
         }
