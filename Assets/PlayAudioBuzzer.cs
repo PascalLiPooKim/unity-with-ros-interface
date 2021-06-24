20b2affa322b2b5ddefbb50102837b14da015f08
PascalLiPooKim <32938312+PascalLiPooKim@users.noreply.github.com>
2021-06-24 17:42:23 +0100

    Add more audio sources around the Husky with different clips.

--- Assets/PlayAudioBuzzer.cs
+++ Assets/PlayAudioBuzzer.cs
@@ -46,30 +46,26 @@ namespace RosSharp.RosBridgeClient
         {
 
             
-                if (Mathf.Pow(closestDistance.data, 1f / 2f) > 0.0f && Mathf.Pow(closestDistance.data, 1f / 2f) < 1.3f)
+                if (Mathf.Pow(closestDistance.data, 1f / 2f) > 0.0f && Mathf.Pow(closestDistance.data, 1f / 2f) < 0.85f)
                 {
                     //audioSource.pitch = Mathf.Min(1.0f/closestDistance.data, 3);
 
                     audioSource.pitch = Mathf.Clamp(1.0f / Mathf.Pow(closestDistance.data, 1f / 9f) - 0.5f, 0.1f, 1.5f);
-                    // audioSource.Play();
+                // audioSource.Play();
                     audioSource.PlayOneShot(impact, 1.0F);
+                    //audioSource.PlayOneShot(impact, Mathf.Pow(closestDistance.data, 1f / 9f));
 
 
                 }
 
                 else
                 {
-                    //audioSource.Stop();
+                    audioSource.Stop();
                     // audioSource.pitch = 1.0f;
-                    audioSource.pitch = 0.0f;
+                    //audioSource.pitch = 0.0f;
                 }
 
                 
-            
-
-            
-
-
             // audioSource.Play();
             isMessageReceived = false;
 
