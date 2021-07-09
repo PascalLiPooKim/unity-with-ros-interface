16789a6392b3be710bbcf0e172130b43ed9f2674
Pascal Weiliam Li Poo Kim <paswei98@gmail.com>
2021-07-09 10:37:59 +0100

    Modify GoalBox.cs to receive messages from ROS side.

--- Assets/PersonalAssets/GoalBox.cs
+++ Assets/PersonalAssets/GoalBox.cs
@@ -2,24 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoalBox : MonoBehaviour
-{
-    //public static Vector3 newPos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Vector3 newPos = new Vector3(1.0f, 0.0f, 4.0f);
-        // gameObject.transform.TransformPoint(newPos);
-    }
+//public class GoalBox : MonoBehaviour
+//{
+//    //public static Vector3 newPos;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        // Vector3 newPos = new Vector3(1.0f, 0.0f, 4.0f);
+//        // gameObject.transform.TransformPoint(newPos);
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//	{
+//        GameObject.Find("Husky_V2").SendMessage("Finish");
+//	}
+//}
+
+
+namespace RosSharp.RosBridgeClient
+{
+    [RequireComponent(typeof(RosConnector))]
+
+    public class GoalBox : UnitySubscriber<MessageTypes.Std.Int32>
+    {
+
+        private bool isMessageReceived;
+        private MessageTypes.Std.Int32 measurementStatus;
+
+        protected override void Start()
+        {       
+            base.Start();
+        }
+
+        void Update()
+		{
+            if (isMessageReceived)
+                StopTimerAndCounter();
+        }
+
+
+        protected override void ReceiveMessage(MessageTypes.Std.Int32 stopMeasuring)
+        {
+            isMessageReceived = true;
+            measurementStatus = stopMeasuring;
+        }
+
+
+        private void StopTimerAndCounter()
+		{
+            if (measurementStatus.data == 1)
+			{
+                GameObject.Find("Husky_V2").SendMessage("Finish");
+            }
+            else if (measurementStatus.data == 0)
+            {
+                print("Ok, it works");
+			}
+		}
     }
 
-    private void OnTriggerEnter(Collider other)
-	{
-        GameObject.Find("Husky_V2").SendMessage("Finish");
-	}
+
 }
