dd698b40346750f7c3b9d0fc84c8307a7fb51ee7
Pascal Weiliam Li Poo Kim <paswei98@gmail.com>
2021-07-11 18:26:53 +0100

    Start timing and counting only when Enter key is pressed. Save result to text file.

--- Assets/PersonalAssets/Meaurement.cs
+++ Assets/PersonalAssets/Meaurement.cs
@@ -2,30 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text;
+using System.IO;
+using System;
 
 public class Meaurement : MonoBehaviour
 {
     public Text timerText;
     public Text keystrokesCounterText;
+    public int IDNumber;
+    public int age;
     private bool targetReached = false;
 
 
-    private float startTime;
+    private float startTime = -1.0f;
     private int keystrokesCount;
+
+    private string completionTimeText;
+    private string numberOfKeyPressedText;
+    
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        InitTimerAndCounter();
+        //InitTimerAndCounter();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    targetReached = true;
+        //    SaveData(completionTimeText, numberOfKeyPressedText, "./CandidatesData.txt");
+        //}
+
         if (targetReached)
 		{
             return;
 		}
 
+        // https://www.youtube.com/watch?v=x-C95TuQtf0
+        if (Input.GetKeyDown(KeyCode.Return))
+		{
+            InitTimerAndCounter();
+        }
+        
         UpdateTimerAndCounter();
         
     }
@@ -36,9 +61,11 @@ public class Meaurement : MonoBehaviour
         keystrokesCount = 0;
     }
 
+    // https://www.youtube.com/watch?v=x-C95TuQtf0
     private void UpdateTimerAndCounter()
 	{
-        float duration = Time.time - startTime;
+        //float duration = Time.time - startTime;
+        float duration = startTime >= 0 ? Time.time - startTime : 0;
 
         string minutes = ((int)duration / 60).ToString();
         string seconds = (duration % 60).ToString("f2");
@@ -51,6 +78,10 @@ public class Meaurement : MonoBehaviour
         }
 
         keystrokesCounterText.text = keystrokesCount.ToString();
+
+        completionTimeText = minutes + ':' + seconds;
+        numberOfKeyPressedText = keystrokesCount.ToString();
+
     }
 
     
@@ -59,6 +90,51 @@ public class Meaurement : MonoBehaviour
 	{
         timerText.color = Color.green;
         keystrokesCounterText.color = Color.yellow;
+        SaveData(completionTimeText, numberOfKeyPressedText, "./CandidatesData.txt");
         targetReached = true;
     }
+
+    //   private void SaveData()
+    //{
+    //       string filePath = getPath();
+    //       //string filePath = "ParticipantsData.csv";
+    //       StreamWriter dataWriter = new StreamWriter(filePath);
+    //       dataWriter.WriteLine("Completion Time, Number of KeyStrokes, Workload Rating");
+    //       dataWriter.WriteLine("Anime is life");
+    //       dataWriter.Flush();
+    //       dataWriter.Close();
+    //}
+
+    // https://www.youtube.com/watch?v=vDpww7HsdnM
+    private void SaveData(string completionTime, string numberOfKeystrokes, string filePath)
+	{
+		try
+		{
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filePath, true))
+			{
+                file.WriteLine(IDNumber.ToString() + ", " + age.ToString() + ", " + completionTime + ", " + numberOfKeystrokes);
+			}
+		}
+        catch(Exception e)
+		{
+            throw new ApplicationException("Lol", e);
+            print("Fail");
+		}
+	}
+
+
+//	private string getPath()
+//	{
+//#if UNITY_EDITOR
+//        return Application.dataPath  + "/ParticipantsData.csv";
+//#elif UNITY_ANDROID
+//        return Application.persistentDataPath+ "ParticipantsData.csv";
+//#elif UNITY_IPHONE
+//        return Application.persistentDataPath+ "/" + "ParticipantsData.csv";
+//#else
+//        return Application.dataPath + "/ParticipantsData.csv";
+//#endif
+//	}
+
+
 }
