34acb0a351b66b3b85d95d77e640f73fe113f3fb
HalfManHalfWookey <harveystedman@gmail.com>
2021-07-23 17:14:12 +0100

    Improve KdTree codes and remove Youbot.

--- Ubuntu 18.04/Docker/pascal_docker/catkin_ws/build/catkin_generated/order_packages.py
+++ Ubuntu 18.04/Docker/pascal_docker/catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/pascal/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/pascal/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/pascal/catkin_ws/devel;/opt/ros/melodic' != '' else []
