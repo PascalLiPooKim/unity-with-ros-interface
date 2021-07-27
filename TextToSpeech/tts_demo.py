470779d47c059ba6bde62451dda88fbfc11da6b9
Pascal Weiliam Li Poo Kim <paswei98@gmail.com>
2021-07-27 10:00:22 +0100

    Improve TTS script with timeout.

--- TextToSpeech/tts_demo.py
+++ TextToSpeech/tts_demo.py
@@ -24,15 +24,15 @@ path = "./DistanceToGoalTTS.txt"
 
 
 # initialize Text-to-speech engine
-engine = pyttsx3.init()
-f = open(path, "r")
-# convert this text to speech
+# engine = pyttsx3.init()
+# f = open(path, "r")
+# # convert this text to speech
 
-while True:
-    text = f.readline()
-    engine.say(text)
-    # play the speech
-    engine.runAndWait()
+# while True:
+#     text = f.readline()
+#     engine.say(text)
+#     # play the speech
+#     engine.runAndWait()
 
 
 # while True:
@@ -45,4 +45,38 @@ while True:
 #     else:
 #         pass
 
+def text_to_speech():
+    engine = pyttsx3.init()
+    f = open(path, "r")
+    # count = 0
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate+20)
+    while True:
+        # if count < 50:
+        text = f.readline()
+        engine.say(text)
+        # play the speech
+        engine.runAndWait()
+        time.sleep(1)
+        # print(count)
+        # count += 1
+        
+        # if engine.isBusy():
+        #     count = 0
+        # engine.runAndWait()
+    # else:
+    #     break
+# initialize Text-to-speech engine
 
+# convert this text to speech
+# count = 0
+if __name__ == '__main__':
+    action_process = Process(target=text_to_speech)
+ 
+    # We start the process and we block for 5 seconds.
+    action_process.start()
+    action_process.join(timeout=300)
+ 
+    # We terminate the process.
+    action_process.terminate()
+    print("Text-to-Speech Function Timeout")
