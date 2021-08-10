bac8a04a6765e509605047e1df847fca64990ffb
Pascal Li Poo Kim <paswei98@gmail.com>
2021-08-10 14:18:34 +0100

    Adjust position and range of audio sources.

---
+++ Assets/MuteOneChannel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteOneChannel : MonoBehaviour
+{
+    public bool right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnAudioFilterRead(float[] data, int channels)
+    {
+        if (right)
+		{
+            for (int i = 0; i < data.Length; i += channels)
+            {
+                data[i] = 0; // mute left channel
+            }
+            //print("yes");
+        }
+		else
+		{
+            for (int i = 0; i < data.Length; i += channels)
+            {
+                data[i + 1] = 0; // mute right channel
+            }
+        }
+        
+    }
+
+}
