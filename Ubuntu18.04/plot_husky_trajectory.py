ee80558fe894ffe9f23a4f5ad89a26722f4d896b
HalfManHalfWookey <harveystedman@gmail.com>
2021-08-24 16:41:22 +0100

    Save rosbags of workload and degraded vision tests.

--- Ubuntu18.04/plot_husky_trajectory.py
+++ Ubuntu18.04/plot_husky_trajectory.py
@@ -7,23 +7,35 @@ import rospkg
 import matplotlib.pyplot as plt
 import sys, os
 from os import listdir
+# import seaborn as sns
+
+
+
+# plt.style.use("ggplot")
+params = {
+   'axes.labelsize': 25,
+   'font.size': 25,
+   'font.family': 'sans-serif',
+   'font.serif': 'Arial',
+   'legend.fontsize': 15,
+   'xtick.labelsize': 25,
+   'ytick.labelsize': 25, 
+   'figure.figsize': [25, 15],
+   'axes.titlesize': 30
+   } 
+plt.rcParams.update(params)
 
 
 #  Function to read rosbags and plot the recorded trajectory of Husky
-def plot_traj():
+def plot_traj_course_2_V():
     rospack = rospkg.RosPack()
-    # path = rospack.get_path("commands_to_unity")
-    # path = rospack.get_path("custom_husky_gazebo")
-
-    # print(path)
-    # Read all .bag files from folder
-    # for file in listdir(path + "/bags"):
-    path_list = []
+    
+    dir_path = "/home/vr-pc/Desktop/courses_2_3_traj_bags/course_2/vision/"
 
     plt.figure()
-    for i in range(2):
-        # bag_path = os.path.join(path + "/bags", file)
-        bag_path = "/home/vr-pc/Desktop/husky_course_3_hard_5FAS_V_66_2021-08-18-14-17-35.bag"
+    for i, file in enumerate(os.listdir(dir_path)):
+        
+        bag_path = dir_path + file
         bag = rosbag.Bag(bag_path)
 
         x = []
@@ -38,14 +50,114 @@ def plot_traj():
 
         # Plot the graph
         
-        plt.plot(x, y, '--', label="Husky Trajectory for Candidate {}".format(i))
+        plt.plot(x, y, '--', label="Husky Trajectory for Candidate {}".format(i + 1))
     plt.legend()
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.title("Trajectory of Husky using Vision only")
+    plt.title("Trajectory of Husky using Vision only of Degraded Vision Experiment")
+    plt.grid()
+    plt.show()
+
+
+def plot_traj_course_2_VA():
+    rospack = rospkg.RosPack()
+    
+    dir_path = "/home/vr-pc/Desktop/courses_2_3_traj_bags/course_2/vision_audio/"
+
+    plt.figure()
+    for i, file in enumerate(os.listdir(dir_path)):
+        
+        bag_path = dir_path + file
+        bag = rosbag.Bag(bag_path)
+
+        x = []
+        y = []
+        z = []
+
+        # Append a and y coordinates of Husky to their respective list
+        for topic, msg, t in bag.read_messages(topics=["/ground_truth/state"]):
+            position = msg.pose.pose.position
+            x.append(position.x)
+            y.append(position.y)
+
+        # Plot the graph
+        
+        plt.plot(x, y, '--', label="Husky Trajectory of Candidate {}".format(i))
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Trajectory of Husky using Vision & Audio for Degraded Vision Experiment")
+    plt.grid()
+    plt.show()
+
+
+def plot_traj_course_3_V():
+    
+    dir_path = "/home/vr-pc/Desktop/courses_2_3_traj_bags/course_3/vision/"
+
+    plt.figure()
+    for i, file in enumerate(os.listdir(dir_path)):
+        
+        bag_path = dir_path + file
+        
+        bag = rosbag.Bag(bag_path)
+
+        x = []
+        y = []
+        z = []
+
+        # Append a and y coordinates of Husky to their respective list
+        for topic, msg, t in bag.read_messages(topics=["/ground_truth/state"]):
+            position = msg.pose.pose.position
+            x.append(position.x)
+            y.append(position.y)
+
+        # Plot the graph
+        
+        plt.plot(x, y, '--', label="Husky Trajectory of Candidate {}".format(i))
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Trajectory of Husky using Vision only in Workload Experiment")
+    plt.grid()
+    plt.show()
+
+
+def plot_traj_course_3_VA():
+    
+    dir_path = "/home/vr-pc/Desktop/courses_2_3_traj_bags/course_3/vision_audio/"
+
+    plt.figure()
+    for i, file in enumerate(os.listdir(dir_path)):
+       
+        bag_path = dir_path + file
+        
+        bag = rosbag.Bag(bag_path)
+
+        x = []
+        y = []
+        z = []
+
+        # Append a and y coordinates of Husky to their respective list
+        for topic, msg, t in bag.read_messages(topics=["/ground_truth/state"]):
+            position = msg.pose.pose.position
+            x.append(position.x)
+            y.append(position.y)
+
+        # Plot the graph
+        
+        plt.plot(x, y, '--', label="Husky Trajectory of Candidate {}".format(i))
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("Trajectory of Husky using Vision & Audio in Workload Experiment")
     plt.grid()
     plt.show()
 
 if __name__ == '__main__':
       rospy.init_node('traj_plotter')
-      plot_traj()
+      plot_traj_course_3_VA()
+      plot_traj_course_3_V()
+      plot_traj_course_2_VA()
+      plot_traj_course_2_V()
+      
